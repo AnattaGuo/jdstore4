3cd1233714d0ad5fdb633b4eb468b29ad431e868
AnattaGuo <mengjunkwok@gmail.com>
2017-07-15 15:48:35 +0800

    add admin and user accoutn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+create_account = User.create([email: 'mjadmin@jdstore4.com', password:'123456', password_confirmation: '123456', is_admin: 'true'])
+puts "Admin account created."
+
+create_account = User.create([email: 'mjuser@jdstore4.com', password:'123456', password_confirmation: '123456', is_admin: 'false'])
+puts "User account created."
