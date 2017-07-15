34e0375b76f559c387391004c52bfbf0beaeb4c8
AnattaGuo <mengjunkwok@gmail.com>
2017-07-15 20:52:16 +0800

    add product to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,19 @@ puts "Admin account created."
 
 create_account = User.create([email: 'mjuser@jdstore4.com', password:'123456', password_confirmation: '123456', is_admin: 'false'])
 puts "User account created."
+
+Product.create!(title: "Cup",
+                description: "Nice Cup",
+                price: 80,
+                quantity: 10,
+                image: open("http://ot4s5c6m1.bkt.clouddn.com/cup-glass-03.jpg")
+                )
+puts "Product created"
+
+Product.create!(title: "Pen",
+                description: "Wow Pen",
+                price: 20,
+                quantity: 100,
+                image: open("http://ot4s5c6m1.bkt.clouddn.com/Pen.jpeg")
+                )
+puts "Product created"
